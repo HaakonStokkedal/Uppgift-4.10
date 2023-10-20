99fa11ccc647e56b34e4a5f0a56d623086a3fc45
hast06001 <hast06001@E-PW024NW1>
2023-10-20 11:11:00 +0200

    Add project files.

---
+++ Uppgift 4.10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Uppgift_4._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många X ska finnas per grupp?");
+            int xVarde = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många O ska finnas per grupp?");
+            int oVarde = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många O-grupper ska det finnas?");
+            int oGrupper = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många rader ska det vara?");
+            int rader = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rader; i++)
+            {
+                for (int j = 0; j < oGrupper; j++) 
+                {
+                    for (int k = 0; k < xVarde; k++)
+                        Console.Write("X");
+                    for (int l = 0; l < oGrupper; l++)
+                        Console.Write("O");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
